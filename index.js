397594905a0fa26fb787298dc5a270c7482cf912
Alessio Biancalana <dottorblaster@gmail.com>
2017-06-20 16:51:15 +0200

    Updated the function to use replace instead of map
    Bumped to ver 1.0.1

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ module.exports = function(payload) {
     if (!payload) { return '' }
 
     return payload
-        .split('')
-        .map((x) => {
-            if (x == 's') return 'f'
-            if (x == 'z') return 'ff'
-            return x
-        })
-        .join('')
+        .replace(/s/g, 'f')
+        .replace(/z/g,'ff')
 }
